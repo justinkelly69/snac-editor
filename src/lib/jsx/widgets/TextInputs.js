97fe186a65d4539b2f6ae0ba147041397035f088
Justin Kelly <justinkelly@justinkelly.net>
2021-09-11 19:02:20 +0200

    TextInputs1

--- src/lib/jsx/widgets/TextInputs.js
+++ src/lib/jsx/widgets/TextInputs.js
@@ -1,42 +1,30 @@
-import styled from 'styled-components'
-import { Colors, Styles } from '..'
+import React from 'react'
 
-export const TextInput = styled.input`
-	border: ${Styles.TextInputBorderWidth} solid ${Colors.TextInputBorderColor};
-	background-color: ${Colors.TextInputBackgroundColor};
-	border-radius: ${Styles.TextInputBorderRadius};
-	font: ${Styles.TextInputFont};
-    font-weight: ${Styles.TextInputFontWeight};
-    height: ${Styles.TextInputHeight};
-    width: ${Styles.TextInputWidth};
-`
-export const NSInput = styled(TextInput)`
-    color: ${Colors.NSColor};
-    border-color: ${Colors.NSColor};
-    width: 5em;
-`
-export const NameInput = styled(TextInput)`
-    color: ${Colors.NameColor};
-    border-color: ${Colors.NameColor};
-    width: 10em;
-`
-export const AttNSInput = styled(TextInput)`
-    color: ${Colors.AttributeNSColor};
-    border-color: ${Colors.AttributeNSColor};
-    width: 5em;
-`
-export const AttNameInput = styled(TextInput)`
-    color: ${Colors.AttributeNameColor};
-    border-color: ${Colors.AttributeNameColor};
-    width: 10em
-`
-export const AttValueInput = styled(TextInput)`
-    color: ${Colors.AttributeValueColor};
-    border-color: ${Colors.AttributeValueColor};
-    width: 10em;
-`
-export const PILangInput = styled(TextInput)`
-    color: ${Colors.PILangColor};
-    border-color: ${Colors.PILangColor};
-    width: 10em;
-`
+export const TextInput = props =>
+    <input type="text"
+        id={props.id}
+        name={props.name}
+        className={props.className}
+        value={props.value}
+        onClick={props.onClick}
+        onChange={props.onChange}
+        onKeyPress={props.onKeyPress}
+    />
+
+export const NSInput = props =>
+    <TextInput className="ns-input" {...props} />
+
+export const NameInput = props =>
+    <TextInput className="name-input" {...props} />
+
+export const AttNSInput = props =>
+    <TextInput className="att-ns-input" {...props} />
+
+export const AttNameInput = props =>
+    <TextInput className="att-name-input" {...props} />
+
+export const AttValueInput = props =>
+    <TextInput className="att-value-input" {...props} />
+
+export const PILangInput = props =>
+    <TextInput className="pi-lang-input" {...props} />
